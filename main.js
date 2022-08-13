9b7010fe5bc1bb8055c8bb171bec4333cf20a100
bdullah386 <bdullahbdullah386@gmail.com>
2022-08-13 10:46:52 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,69 @@
+// let myArray =['H','e','l','l','o',' ','W','o','r','l','d']
+
+// for(let item of myArray){
+//     console.log());
+// }
+
+// let number = {
+//     senin: 55400,
+//     selasa: 61600,
+//     rabu: 49300,
+//     kamis: 53200,
+//     jumat: 54500,
+//     sabtu: 51500,
+//     minggu: 70000
+// }
+
+// let text = " ";
+// for(let x in number){
+//     text += number[x]
+    
+//     console.log(number[x]);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let z = [
+//     {
+//     jam:'12:10',
+//     masuk:'tidak dikenal'
+//     }
+// ]
+
+// let a =  {
+//     jam:'12:14',
+//     masuk:'tidak dikenal'
+//     }
+
+// console.log(z.push(a));
+
+
+// console.table(z)
+
+
+
+// question=true
+// // while (question) {
+    
+//     alert("masukkan pilihan anda")
+
+//     alert("")
+
+//     let comp = Math.round(Math.random())
+
+//     console.log(player);
+
+    
+
+
+// // }
